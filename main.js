ec55de9bd17d2887e283d66b14d9d9fd094fbf15
June <sungj1207@gmail.com>
2023-06-01 15:09:21 +0900

    문자

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('Hello world')
+// JS 데이터
+// Google : String mdn
+
+// String: "", '', ``
+// Number
+// Boolean: true, false
+// undefined
+// null
+// Array: []
+// Object: {}
+
+// String.prototype.indexOf()
+// 0부터 시작하는 index값 반환(일치하는 값이 없으면 -1 반환)
+
+const result = 'Hello world!'.indexOf('world')
+console.log(result) // 6
+
+// const str = '0123'
+// console.log(str.length)
+
+// const str = 'Hello world!'
+// console.log(str.indexOf('world')) // 6
+
+// Boolean
+// console.log(str.indexOf('June') !== -1) // false
+
+// Slice
+// console.log(str.slice(0, 3)) // Hel (0~2번까지만 출력)
+
+// Replace
+// console.log(str.replace('world', 'JUNE')) // Hello JUNE!
+// console.log(str.replace(' world!', '')) // Hello
+
+// 정규표현식
+// const str = 'thesecon@gmail.com'
+// console.log(str.match(/.+(?=@)/)[0]) // domain 주소 제거되고 출력
+
+// 공백제거 trim
+const str = '    Hello world  '
+console.log(str.trim())
