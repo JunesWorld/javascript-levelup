247bedf6eb875a7fee6b5e9f2121814c327ae1a6
June <sungj1207@gmail.com>
2023-06-01 15:45:10 +0900

    숫자와 수학

--- main.js
+++ main.js
@@ -36,5 +36,38 @@ console.log(result) // 6
 // console.log(str.match(/.+(?=@)/)[0]) // domain 주소 제거되고 출력
 
 // 공백제거 trim
-const str = '    Hello world  '
-console.log(str.trim())
+// const str = '    Hello world  '
+// console.log(str.trim())
+
+
+/**
+ * 숫자
+ * Google : Math mdn
+ */
+
+const pi = 3.14159265358979
+console.log(pi)
+
+const str = pi.toFixed(2) 
+console.log(str) // 3.14
+console.log(typeof str) // string
+
+// 전역함수 : parseInt, parseFloat
+const integer = parseInt(str)
+const float = parseFloat(str)
+console.log(integer) // 3
+console.log(float) // 3.14
+console.log(typeof integer, typeof float) // number number
+
+// 절대값 abs
+console.log('abs: ', Math.abs(-12)) // 12
+console.log('min: ', Math.min(2, 8)) // 2
+console.log('max: ', Math.max(2, 8)) // 8
+// 올림 ceil
+console.log('ceil: ', Math.ceil(3.14)) // 4
+// 내림 floor
+console.log('floor: ', Math.floor(3.14)) // 3
+// 반올림 round
+console.log('round: ', Math.round(3.14)) // 3
+// random 0.xxx
+console.log('random: ', Math.random()) 
