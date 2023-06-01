a2b4ad65b886ee678843b2493a1fca02a898ed64
June <sungj1207@gmail.com>
2023-06-01 17:39:32 +0900

    배열(2)

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // JS 데이터
+
+/**
+ * 화살표 함수
+ * = 다른 로직 없이 return을 통해 하나의 실행문만 있으면 중괄호 생략하고 바로 실행문 입력
+ */
 // Google : String mdn
 
 // String: "", '', ``
@@ -88,6 +93,7 @@ console.log(numbers.length) // 4
 console.log(fruits.length) // 3
 console.log([1, 2].length) // 2
 
+
 // .concat() 
 // = 두 개의 배열 데이터를 병합해서 새로운 배열 데이터를 반환, 
 // 원본 배열 데이터 손상x
@@ -95,12 +101,14 @@ console.log(numbers.concat(fruits))
 console.log(numbers)
 console.log(fruits)
 
+
 // .forEach()
 // callback 함수
 // 새로 반환 X
 fruits.forEach(function (element, index, array) {
   console.log(element, index, array)
 })
+
 
 // .map() = 새로운 배열[]로 출력
 const a = fruits.forEach((fruit, index) => {
@@ -126,3 +134,54 @@ const b = fruits.map((fruit, index) => ({
     name: fruit
   }))
 console.log(b) // [{id: 0, name: "Apple"}, {id: 1, name: "Banana"}, {id: 2, name: "Cherry"}]
+
+
+// .filter()
+// const c = numbers.map(number => {
+//   return number < 3
+// })
+// console.log(c) // [true, true, false, false]
+const c = numbers.map(number => number < 3)
+console.log(c) // [true, true, false, false]
+
+// const d = numbers.filter(number => {
+//   return number < 3
+// })
+// console.log(d) // [1, 2]
+const d = numbers.filter(number => number < 3)
+console.log(d) // [1, 2]
+
+
+// .find() .findIndex()
+// 정규표현식(/^B/) = ^(시작)
+const e = fruits.find(fruit => /^B/.test(fruit)) 
+console.log(e) // Banana
+
+const f = fruits.findIndex(fruit => /^B/.test(fruit)) 
+console.log(f) // 1
+
+
+// .includes()
+// const a = numbers.includes(3)
+// console(a) // true
+
+// const b = fruits.includes('JUNE')
+// console.log(b) // false
+
+
+// .push() = 가장 뒷쪽에 삽입 
+// .unshift() = 가장 앞쪽에 삽입
+// .reverse()
+// .splice(index번호, 지울 갯수) / .splice(index번호, 지울 갯수, 끼워 넣을 item)
+// 원본 수정됨 주의!
+numbers.push(5)
+console.log(numbers) // [1, 2, 3, 4, 5]
+
+numbers.unshift(0)
+console.log(numbers) // [0, 1, 2, 3, 4, 5]
+
+numbers.reverse()
+fruits.reverse()
+
+console.log(numbers)
+console.log(fruits)
